e81f355d5fccf8517bf9e67285afcd5b143e931a
Wilfred <wilfredcloudspace@gmail.com>
2023-01-19 10:53:47 +0100

    Create main and other functions  definition

---
+++ main.c
@@ -0,0 +1,85 @@
+#include "queue";
+
+/**
+ * main - Implements queue using two stacks
+ * 
+ * Return: Always 0
+*/
+int main(void)
+{
+
+}
+
+/**
+ * enque - Add item to queue
+ * 
+ * Return: Nothing
+*/
+void enque(int x)
+{
+
+}
+
+/**
+ * deque - Remove item from queue
+ * 
+ * Return: Value of item removed
+*/
+int deque(int x)
+{
+    
+}
+
+/**
+ * display - Print all items in queue
+ * 
+ * Return: Nothing
+*/
+void display(int x)
+{
+
+}
+
+
+
+
+/**
+ * pop1 - Removes the top item on stack1
+ * 
+ * Return: Value of item
+*/
+int pop1()
+{
+
+}
+
+/**
+ * push1 - Adds item to the top of stack1
+ * 
+ * Return: Nothing
+*/
+void push1(int x)
+{
+
+}
+
+/**
+ * pop2 - Removes the top item on stack2
+ * 
+ * Return: Value of item
+*/
+int pop2()
+{
+
+}
+
+/**
+ * push2 - Adds item to the top of stack2
+ * 
+ * Return: Nothing
+*/
+void push2(int x)
+{
+
+}
+
